26cac34818a64a0a807404b7384337d7762fd200
reklamingo <arifkurtulmuslar@gmail.com>
2025-05-05 21:38:00 +0300

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
@@ -8,8 +11,8 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Quicksand', 'sans-serif'],
-      }
+      },
     },
   },
   plugins: [],
-};
+}
